7793c35741d18c64ec352b321a5c6bef50173911
infullstack <infullstack@163.com>
2016-12-22 21:25:51 +0800

    first commit

--- sidebar.php
+++
@@ -1,7 +0,0 @@
-<div id="sidebar" class="widget-area" role="complementary">
-<?php if ( is_active_sidebar( 'sidebar' ) ) : ?>
-	<div id="secondary" class="widget-area" role="complementary">
-		<?php dynamic_sidebar( 'sidebar' ); ?>
-	</div><!-- #secondary -->
-<?php endif; ?>
-</div><!-- #sidebar .widget-area -->
