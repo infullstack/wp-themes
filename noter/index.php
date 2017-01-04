8f352cb3ee22d3009afa9be01e7de4c14b1069c5
infullstack <infullstack@163.com>
2017-01-04 15:38:21 +0800

    增加JiaThis分享

--- noter/index.php
+++ noter/index.php
@@ -11,12 +11,47 @@
 					the_post();
 					the_content();
 				} }; ?>
+				
 				<div class="meta">
 					<p>— 转载本站文章请注明作者和出处<a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a> ，请勿用于任何商业用途</p>
 					<p>— 于 <?php the_time('Y年m月d日') ?> ，共写了 <?php echo count_words(get_the_content())?> 字；</p>
 					<p>— 本文共有 <?php echo count(get_the_tags(),0);?> 个<?php the_tags(); ?></p>
 				</div>
+				<!-- JiaThis Button BEGIN -->
+				<div class="jiathis_style">
+					<a class="jiathis_button_weixin"></a>
+					<a class="jiathis_button_tsina"></a>	
+					<a class="jiathis_button_douban"></a>
+					<a class="jiathis_button_tqq"></a>	
+					<a class="jiathis_button_cqq"></a>
+					<a class="jiathis_button_instapaper"></a>	
+					<a class="jiathis_button_twitter"></a>
+					<a class="jiathis_button_ydnote"></a>	
+					<a class="jiathis_button_gmail"></a>
+					<a class="jiathis_button_cyzone"></a>
+					<a class="jiathis_button_tumblr"></a>
+					<a class="jiathis_button_delicious"></a>	
+					<a class="jiathis_button_linkedin"></a>
+					<a class="jiathis_button_email"></a>
+					<a class="jiathis_button_print"></a>
+					<a class="jiathis_button_copy"></a>
+					<a class="jiathis_button_fav"></a>
+				</div>
+				<script type="text/javascript" src="http://v3.jiathis.com/code_mini/jia.js" charset="utf-8"></script>
+				<!-- JiaThis Button END -->
 			</article>
+			<?php if(get_the_author_meta('alipay')||get_the_author_meta('wechatpay')){ ?>
+				<div class="reward">
+				赏
+				<ul>
+				<?php if(get_the_author_meta('alipay')){ ?>
+				<li><img src="<?php the_author_meta('alipay');?>">用支付宝打我</li>
+				<?php }; if(get_the_author_meta('wechatpay')){ ?>
+				<li><img src="<?php the_author_meta('wechatpay');?>">用微信打我</li>
+				<?php };?>
+				</ul>
+				</div>
+			<?php }?>
 		<?php } else { ?>
 			<article class="page single" itemscope itemtype="http://schema.org/BlogPosting">
 				<?php if (have_posts()) { while (have_posts()) {
@@ -27,46 +62,33 @@
 		<?php } ?>
 		<?php setPostViews(get_the_ID()); ?>
 		
-<section class="ending">
-<div class="about">
-<a><?php echo get_avatar(get_the_author_email(),'80');?></a>
-<span><?php the_author_meta('description');?></span>
-</div>
-<?php if(get_the_author_meta('weibo')||get_the_author_meta('tencent')||get_the_author_meta('douban')||get_the_author_meta('zhihu')||get_the_author_meta('github')){ ?>
-<ul class="sns">
-<?php if(get_the_author_meta('weibo')){ ?>
-<li class="weibo"><a href="<?php the_author_meta('weibo');?>" target="_blank"><i class="fa fa-weibo"></i></a></li>
-<?php }; if(get_the_author_meta('tencent')){ ?>
-<li class="tencent"><a href="http://wpa.qq.com/msgrd?v=3&uin=<?php the_author_meta('tencent');?>&site=qq&menu=yes" target="_blank"><i class="fa fa-qq"></i></a></li>
-<?php }; if(get_the_author_meta('douban')){ ?>
-<li class="douban"><a href="<?php the_author_meta('douban');?>" target="_blank"><i class="fa fa-douban"></i></a></li>
-<?php }; if(get_the_author_meta('zhihu')){ ?>
-<li class="zhihu"><a href="<?php the_author_meta('zhihu');?>" target="_blank"><i class="fa fa-zhihu"></i></a></li>
-<?php }; if(get_the_author_meta('github')){ ?>
-<li class="github"><a href="<?php the_author_meta('github');?>" target="_blank"><i class="fa fa-github"></i></a></li>
-<?php };?>
-<?php if(get_the_author_meta('twitter')){ ?>
-	<li class="twitter"><a href="<?php the_author_meta('twitter');?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
-<?php };?>
-	<li class="rss"><a href="<?php bloginfo('url'); ?>/feed" target="_blank"><i class="fa fa-rss-square"></i></a></li>
-</ul>
-<?php };?>
-    
-<?php if(get_the_author_meta('alipay')||get_the_author_meta('wechatpay')){ ?>
-<div class="reward">
-赏
-<ul>
-<?php if(get_the_author_meta('alipay')){ ?>
-<li><img src="<?php the_author_meta('alipay');?>">用支付宝打我</li>
-<?php }; if(get_the_author_meta('wechatpay')){ ?>
-<li><img src="<?php the_author_meta('wechatpay');?>">用微信打我</li>
-<?php };?>
-</ul>
-</div>
-<?php }?>
-</section>        
-	<?php comments_template(); }
-	else { if (have_posts()): $x=0; while (have_posts()): the_post(); $x=$x+1;?>
+		<section class="ending">
+		<div class="about">
+		<a><?php echo get_avatar(get_the_author_email(),'80');?></a>
+		<span><?php the_author_meta('description');?></span>
+		</div>
+		<?php if(get_the_author_meta('weibo')||get_the_author_meta('tencent')||get_the_author_meta('douban')||get_the_author_meta('zhihu')||get_the_author_meta('github')){ ?>
+		<ul class="sns">
+		<?php if(get_the_author_meta('weibo')){ ?>
+		<li class="weibo"><a href="<?php the_author_meta('weibo');?>" target="_blank"><i class="fa fa-weibo"></i></a></li>
+		<?php }; if(get_the_author_meta('tencent')){ ?>
+		<li class="tencent"><a href="http://wpa.qq.com/msgrd?v=3&uin=<?php the_author_meta('tencent');?>&site=qq&menu=yes" target="_blank"><i class="fa fa-qq"></i></a></li>
+		<?php }; if(get_the_author_meta('douban')){ ?>
+		<li class="douban"><a href="<?php the_author_meta('douban');?>" target="_blank"><i class="fa fa-douban"></i></a></li>
+		<?php }; if(get_the_author_meta('zhihu')){ ?>
+		<li class="zhihu"><a href="<?php the_author_meta('zhihu');?>" target="_blank"><i class="fa fa-zhihu"></i></a></li>
+		<?php }; if(get_the_author_meta('github')){ ?>
+		<li class="github"><a href="<?php the_author_meta('github');?>" target="_blank"><i class="fa fa-github"></i></a></li>
+		<?php };?>
+		<?php if(get_the_author_meta('twitter')){ ?>
+			<li class="twitter"><a href="<?php the_author_meta('twitter');?>" target="_blank"><i class="fa fa-twitter"></i></a></li>
+		<?php };?>
+			<li class="rss"><a href="<?php bloginfo('url'); ?>/feed" target="_blank"><i class="fa fa-rss-square"></i></a></li>
+		</ul>
+		<?php };?>
+		</section>        
+	<?php comments_template(); 
+	} else { if (have_posts()): $x=0; while (have_posts()): the_post(); $x=$x+1;?>
 		<?php if($x==1){?>
 			<article class="post post-list" itemscope="" itemtype="http://schema.org/BlogPosting">
 				<h2 itemprop="name headline" class="title"><a href="<?php the_permalink();?>"><i class="fa fa-star" style="font-size: 16px;"></i>&nbsp;&nbsp;<?php the_title();?></a></h2>
